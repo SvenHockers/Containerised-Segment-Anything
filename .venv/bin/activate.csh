ebf0f75d972a3fb17fd75397269b3468a48292d3
SvenHockers <90685770+SvenHockers@users.noreply.github.com>
2023-04-11 14:38:19 +0200

    Docker setup

--- .venv/bin/activate.csh
+++ .venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/svenhockers/Desktop/VScode/Containerised-Segment-Anything/.pyVenv"
+setenv VIRTUAL_ENV "/Users/svenhockers/Desktop/VScode/Containerised-Segment-Anything/.venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -17,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(.pyVenv) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(.pyVenv) "
+    set prompt = "(.venv) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(.venv) "
 endif
 
 alias pydoc python -m pydoc
--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/svenhockers/Desktop/VScode/Containerised-Segment-Anything/.pyVenv"
+set -gx VIRTUAL_ENV "/Users/svenhockers/Desktop/VScode/Containerised-Segment-Anything/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -53,7 +53,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(.pyVenv) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(.venv) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -62,5 +62,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(.pyVenv) "
+    set -gx VIRTUAL_ENV_PROMPT "(.venv) "
 end
